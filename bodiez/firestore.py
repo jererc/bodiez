d18a4d5c8c94f9ed9c3b8452c3367567af4ed3f0
jererc <jererc@gmail.com>
2024-11-30 11:32:38 +0400

    do epic things

--- bodiez/firestore.py
+++ bodiez/firestore.py
@@ -1,5 +1,4 @@
 import os
-from pprint import pformat
 from urllib.parse import quote
 
 from google.cloud import firestore
@@ -24,26 +23,20 @@ class FireStore:
         return quote(url, safe='')
 
     def _get_or_set(self, url):
-        @firestore.transactional
-        def get_or_set(transaction):
-            doc_id = self._get_doc_id(url)
-            doc_ref = self.col.document(doc_id)
-            doc = doc_ref.get()
-            if doc.exists:
-                logger.debug(f'loaded stored doc {doc_ref.id}:\n'
-                    f'{pformat(doc.to_dict())}')
-                return {'ref': doc_ref, 'data': doc.to_dict()}
-            data = {'url': url, 'titles': []}
-            transaction.set(doc_ref, data)
-            return {'ref': doc_ref, 'data': data}
-
-        return get_or_set(self.db.transaction())
+        doc_id = self._get_doc_id(url)
+        doc_ref = self.col.document(doc_id)
+        doc = doc_ref.get()
+        if doc.exists:
+            logger.debug(f'loaded stored doc {doc_ref.id}')
+            return {'ref': doc_ref, 'data': doc.to_dict()}
+        data = {'url': url, 'titles': []}
+        doc_ref.set(data)
+        return {'ref': doc_ref, 'data': data}
 
     def get(self, url):
         return self._get_or_set(url)
 
     def update_ref(self, doc_ref, titles):
         doc_ref.update({'titles': titles})
-        logger.debug(f'updated doc {doc_ref.id} titles:\n'
-            f'{pformat(titles)}')
+        logger.debug(f'updated doc {doc_ref.id} with {len(titles)}')
         return doc_ref
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bodiez',
-    version='2024.11.30.105504',
+    version='2024.11.30.113237',
     author='jererc',
     author_email='jererc@gmail.com',
     url='https://github.com/jererc/bodiez',
