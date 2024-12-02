ec03ebe5278264b1b81833ec0240be50578cad78
jererc <jererc@gmail.com>
2024-12-02 08:06:06 +0400

    do epic things

--- bootstrap/user_settings.py
+++ bootstrap/user_settings.py
@@ -4,10 +4,10 @@ GOOGLE_CREDS = os.path.join(os.path.expanduser('~'), 'creds.json')
 URLS = [
     {
         'url': 'https://1337x.to/cat/Games/1/',
-        'id': 'games',
     },
     {
         'url': 'https://1337x.to/cat/Movies/1/',
         'id': 'movies',
+        'update_delta': 8 * 3600,
     },
 ]
