1becf770a7fc57b7e487d8aebf2c109238abac0d
jererc <jererc@gmail.com>
2024-12-03 06:45:41 +0400

    do epic things

--- bootstrap/user_settings.py
+++ bootstrap/user_settings.py
@@ -1,43 +1,36 @@
 import os
 
+
 GOOGLE_CREDS = os.path.join(os.path.expanduser('~'), 'google_creds.json')
 
-params_1337x = {
-    'xpath': '//table/tbody/tr/td[1]/a[2]',
-}
-params_geforce_news = {
-    'xpath': '//div[contains(@class, "article-title-text")]/a',
-}
-params_lexpress = {
-    'parent_xpath': '//div[contains(@class, "card-row")]',
-    'children_xpaths': [
-        './/div[contains(@class, "title-holder")]/h2',
-        './/address',
-        './/div[contains(@class, "card-foot-price")]/strong/a',
-    ],
-}
+xpath_1337x = '//table/tbody/tr/td[1]/a[2]'
 
 URLS = [
     {
         'url': 'https://1337x.to/cat/Games/1/',
         'id': 'games',
-        'params': params_1337x,
+        'xpath': xpath_1337x,
     },
     {
         'url': 'https://1337x.to/cat/Anime/1/',
         'id': 'anime',
-        'params': params_1337x,
+        'xpath': xpath_1337x,
     },
     {
         'url': 'https://www.nvidia.com/en-us/geforce/news/',
         'id': 'geforce-news',
-        'params': params_geforce_news,
+        'xpath': '//div[contains(@class, "article-title-text")]/a',
         'update_delta': 12 * 3600,
     },
     {
         'url': 'https://www.lexpressproperty.com/en/buy-mauritius/residential_land/riviere_noire-la_gaulette-la_preneuse-tamarin/?price_max=8%2C000%2C000&currency=MUR&filters%5Bland_unit%5D%5Beq%5D=m2',
         'id': 'land-west',
-        'params': params_lexpress,
+        'parent_xpath': '//div[contains(@class, "card-row")]',
+        'children_xpaths': [
+            './/div[contains(@class, "title-holder")]/h2',
+            './/address',
+            './/div[contains(@class, "card-foot-price")]/strong/a',
+        ],
         'update_delta': 8 * 3600,
     },
 ]
