d7a94269cac542409ea0b87681b579a7429f96b3
jererc <jererc@gmail.com>
2024-12-05 07:15:14 +0400

    do epic things

--- bodiez/collector.py
+++ bodiez/collector.py
@@ -37,7 +37,7 @@ class URLItem:
     parent_xpath: str = None
     child_xpaths: List[str] = field(default_factory=list)
     multi_element_delimiter: str = ', '
-    max_notif: int = 4
+    max_notif: int = 3
     max_bodies_per_notif: int = 1
     cleaner: any = clean_body
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bodiez',
-    version='2024.12.05.065403',
+    version='2024.12.05.071513',
     author='jererc',
     author_email='jererc@gmail.com',
     url='https://github.com/jererc/bodiez',
