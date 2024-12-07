fdf4962ec6bcbab5afb7beb6c97d9f483d5ba788
jererc <jererc@gmail.com>
2024-12-07 09:11:44 +0400

    do epic things

--- bodiez/parsers/custom.py
+++ bodiez/parsers/custom.py
@@ -3,9 +3,6 @@ import requests
 from bodiez.parsers.base import BaseParser
 
 
-URL = 'https://gfwsl.geforce.com/services_toolkit/services/com/nvidia/services/AjaxDriverService.php?func=DriverManualLookup&psid=120&pfid=942&osID=57&languageCode=1033&beta=0&isWHQL=1&dltype=-1&dch=1&upCRD=0&qnf=0&ctk=null&sort1=1&numberOfResults=1'
-
-
 class GeforceDriverVersionParser(BaseParser):
     id = 'geforce_driver_version'
 
@@ -13,7 +10,8 @@ class GeforceDriverVersionParser(BaseParser):
         return self.url_item.url == 'geforce-driver-version'
 
     def parse(self):
-        res = requests.get(URL)
+        url = 'https://gfwsl.geforce.com/services_toolkit/services/com/nvidia/services/AjaxDriverService.php?func=DriverManualLookup&psid=120&pfid=942&osID=57&languageCode=1033&beta=0&isWHQL=1&dltype=-1&dch=1&upCRD=0&qnf=0&ctk=null&sort1=1&numberOfResults=1'
+        res = requests.get(url)
         if res.status_code != 200:
             raise Exception(f'error {res.status_code}')
         yield res.json()['IDS'][0]['downloadInfo']['Version']
