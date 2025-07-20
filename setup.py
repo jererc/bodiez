7c60f1bc0f43b1d8bc1843115412281007d00296
jererc <jererc@gmail.com>
2025-07-20 14:21:58 +0400

    do epic things

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bodiez',
-    version='2025.07.17.135734',
+    version='2025.07.20.142158',
     author='jererc',
     author_email='jererc@gmail.com',
     url='https://github.com/jererc/bodiez',
